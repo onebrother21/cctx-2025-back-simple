473ae66b566688736615b4497347a17cb732ab5e
onebrother21 <service.onebrother@gmail.com>
2026-05-05 08:29:41 -0500

    'rebuild'

--- src/middleware/configure-session.handler.ts
+++ src/middleware/configure-session.handler.ts
@@ -6,28 +6,28 @@ import Utils from "@utils";
 // COOKIES & SESSIONS
 const cookieSecret = process.env.COOKIE_SECRET || 'myCookieSecret';
 
-const mongoStore = MongoStore.create({
-  collectionName:"ultimate-sessions",
-  dbName:process.env.DB_NAME,
-  mongoUrl:process.env[`DB_URI_${/live/i.test(Utils.env())?"LIVE":"LOCAL"}`],
-  autoRemove: 'interval',
-  autoRemoveInterval: 30 // In minutes
-});
-const sessionOpts:SessionOptions = {
-  name:"my-ultimate-session",
-  secret:cookieSecret,
-  saveUninitialized:true,
-  resave:false,
-  cookie:{
-    sameSite:"strict",
-    path: '/',
-    secure:process.env.NODE_ENV === 'production',
-    httpOnly:true,
-    maxAge:30 * 60 * 1000,
-  },
-  store:mongoStore,
+export const ConfigureSession:() => IHandler = () => {
+  const mongoStore = MongoStore.create({
+    collectionName:"ultimate-sessions",
+    dbName:process.env.DB_NAME,
+    mongoUrl:process.env[`DB_URI_${/live/i.test(Utils.env())?"LIVE":"LOCAL"}`],
+    autoRemove: 'interval',
+    autoRemoveInterval: 30 // In minutes
+  });
+  const sessionOpts:SessionOptions = {
+    name:"my-ultimate-session",
+    secret:cookieSecret,
+    saveUninitialized:true,
+    resave:false,
+    cookie:{
+      sameSite:"strict",
+      path: '/',
+      secure:process.env.NODE_ENV === 'production',
+      httpOnly:true,
+      maxAge:30 * 60 * 1000,
+    },
+    store:mongoStore,
+  };
+  return session(sessionOpts) as any;
 };
-
-export const ConfigureSession:() => IHandler = () => session(sessionOpts) as any;
 export default ConfigureSession;
-export {sessionOpts};
